06f8df5cfb6e9de835d242ec1cc9aeb2f49fdb37
KoleLunger <kole2021864@live.ytech.edu>
2019-12-18 10:55:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static("public"));
 
-const serveraddress = '192.168.56.1'
+const serveraddress = '192.168.24.161:3001'
 app.listen(3000, function() {
   console.log('Listening on port ' + 3000 + '.');
 });
 
 app.get('/', function(req, res, err) {
   let options = {
-    url: 'https://api.github.com/repos/koleLunger/JSFBrowser',
+    url: 'https://api.github.com/repos/Yadi202/Ultimate-Browser',
     headers: {
       'User-Agent': 'request'
     }
